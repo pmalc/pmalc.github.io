d89f31710435e9f018b1f742cdd662f9bdadce96
pmalc <pmalc990@gmail.com>
2024-01-08 21:30:46 +0100

    Revert "Changes for test emails"
    This reverts commit 176a15875871eab25833f9cd2cabc149bbd209bc.

--- api/send_email.php
+++ api/send_email.php
@@ -1,43 +1,52 @@
-<?php
+curl \
+--ssl-reqd \
+--url 'smtp://sandbox.smtp.mailtrap.io:2525' \
+--user 'bf4498e1e0e2e1:cc4662948f28eb' \
+--mail-from from@example.com \
+--mail-rcpt to@example.com \
+--upload-file - <<EOF
+From: Magic Elves <from@example.com>
+To: Mailtrap Inbox <to@example.com>
+Subject: You are awesome!
+Content-Type: multipart/alternative; boundary="boundary-string"
 
-require __DIR__.'../../vendor/autoload.php';
+--boundary-string
+Content-Type: text/plain; charset="utf-8"
+Content-Transfer-Encoding: quoted-printable
+Content-Disposition: inline
 
-header('Content-type: application/json');
-header('Access-Control-Allow-Headers: Content-Type');
-header("Access-Control-Allow-Origin: *");
+Congrats for sending test email with Mailtrap!
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+If you are viewing this email in your inbox =E2=80=93 the integration works.
+Now send your email using our SMTP server and integration of your choice!
 
+Good luck! Hope it works.
 
-$mail = new PHPMailer();
-$mail->isSMTP();
-$mail->Host = 'sandbox.smtp.mailtrap.io';
-$mail->SMTPAuth = true;
-$mail->Port = 2525;
-$mail->Username = 'bf4498e1e0e2e1';
-$mail->Password = 'cc4662948f28eb';
-$mail->SMTPDebug = 2;
+--boundary-string
+Content-Type: text/html; charset="utf-8"
+Content-Transfer-Encoding: quoted-printable
+Content-Disposition: inline
 
-try {                     
-    //Recipients
-    $mail->setFrom('info@mailtrap.io');
-    $mail->addAddress('info@mailtrap.io');     
+<!doctype html>
+<html>
+  <head>
+    <meta http-equiv=3D"Content-Type" content=3D"text/html; charset=3DUTF-8">
+  </head>
+  <body style=3D"font-family: sans-serif;">
+    <div style=3D"display: block; margin: auto; max-width: 600px;" class=3D"main">
+      <h1 style=3D"font-size: 18px; font-weight: bold; margin-top: 20px">Congrats for sending test email with Mailtrap!</h1>
+      <p>If you are viewing this email in your inbox =E2=80=93 the integration works.</p>
+      <img alt=3D"Inspect with Tabs" src=3D"https://assets-examples.mailtrap.io/integration-examples/welcome.png" style=3D"width: 100%;">
+      <p>Now send your email using our SMTP server and integration of your choice!</p>
+      <p>Good luck! Hope it works.</p>
+    </div>
+    <!-- Example of invalid for email html/css, will be detected by Mailtrap: -->
+    <style>
+      .main { background-color: white; }
+      a:hover { border-left-width: 1em; min-height: 2em; }
+    </style>
+  </body>
+</html>
 
-    //Content
-    $mail->isHTML(true);                            
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    if($mail->send()){
-        $message = 'Message has been sent.';
-    }else{
-        $message = 'Message has not been sent.';
-    }
-} catch (Exception $e) {
-    $message = "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
-
-echo json_encode(array('message' => $message));
+--boundary-string--
+EOF
